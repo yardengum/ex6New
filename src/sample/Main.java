e6f2c7d634a94246acdf79fe2689be3d428d21e5
yardengum <yardengum@gmail.com>
2016-01-13 10:08:54 -0800

    connect ex6 to bitbucket

---
+++ src/sample/Main.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static void main (String[] args) {
+        launch(args);
+    }
+    @Override
+
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Scene scene = new Scene(root, 300, 250);
+        scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+}
---
+++ src/sample/mainController.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class mainController {
+
+    @FXML
+    Button btnLogoApp;
+
+
+}
